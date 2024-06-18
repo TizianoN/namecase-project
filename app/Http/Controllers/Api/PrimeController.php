cdb8537bb62cd7d7076a9a80bb7db83e00dda2c6
TizianoN <hello@tizianonicolai.com>
2024-06-18 11:35:51 +0200

    add primes endpoint

---
+++ app/Http/Controllers/Api/PrimeController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PrimeController extends Controller
+{
+  /**
+   * Returns next prime number.
+   */
+  public function nextPrime(int $user_number)
+  {
+    $applicantPrime = $user_number + 1;
+
+    while (!$this->isPrime($applicantPrime))
+      $applicantPrime++;
+
+    return response()->json(['nextPrime' => $applicantPrime]);
+  }
+
+  /**
+   * Returns previous prime number. If none is found returns false
+   */
+  public function prevPrime(int $user_number)
+  {
+    $applicantPrime = $user_number - 1;
+
+    if ($applicantPrime < 2) {
+      return response()->json(['prevPrime' => false]);
+    }
+
+    while (!$this->isPrime($applicantPrime))
+      $applicantPrime--;
+
+    return response()->json(['prevPrime' => $applicantPrime]);
+  }
+
+  /**
+   * Returns previous prime number. If none is found returns false
+   */
+  public function primesBetween(int $min_number, int $max_number)
+  {
+    $primes = [];
+
+    for ($i = $min_number; $i < $max_number; $i++) {
+      if ($this->isPrime($i)) {
+        $primes[] = $i;
+      }
+    }
+
+    return response()->json(['primesBetween' => $primes]);
+  }
+
+  /**
+   * Checks if a number is prime.
+   */
+  private function isPrime($number)
+  {
+    // no number before 2 is prime
+    if ($number < 2)
+      return false;
+
+    // 2 is the only even prime
+    if ($number == 2)
+      return true;
+
+    // any other even number is not prime
+    if ($number % 2 == 0)
+      return false;
+
+    // let's check if $number is divisible by other numbers
+    // starting by 3 (previouses are already tested)
+    $counter = 3;
+
+    // until we get to half our $number (nexts are impossible)
+    while ($counter < $number / 2) {
+
+      // if we can divide by the counter, tested $number is not prime 
+      if ($number % $counter == 0)
+        return false;
+
+      // we can increment counter by 2 (because we already excluded even numbers)
+      $counter = $counter + 2;
+    }
+
+    // if we got this far the number is prime for sure
+    return true;
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PrimeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
+
+Route::get('/next-prime/{user_number}', [PrimeController::class, 'nextPrime']);
+Route::get('/prev-prime/{user_number}', [PrimeController::class, 'prevPrime']);
+Route::get('/primes-between/{min_number}/{max_number}', [PrimeController::class, 'primesBetween']);
